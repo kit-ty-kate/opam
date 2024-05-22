787656c1e6478a09e85eb34349344128b4863d80
Kate <kit-ty-kate@outlook.com>
2024-05-22 23:32:13 +0100

    untested: make -Dev work

--- shell/install.ps1
+++ shell/install.ps1
@@ -17,29 +17,23 @@ Function GetArch {
 
 Function BinSHA512 {
   param (
-    [string]$OpamBin
+    [string]$OpamBinName
   )
 
-  switch ($OpamBin) {
+  switch ($OpamBinName) {
     "opam-2.2.0-beta2-x86_64-windows.exe" { "74f034ccc30ef0b2041283ff125be2eab565d4019e79f946b515046c4c290a698266003445f38b91321a9ef931093651f861360906ff06c076c24d18657e2aaf" }
     Default { throw "no sha" }
   }
 }
 
-$Tag = $Version -creplace "~", "-"
-$Arch = GetArch
-$OS = "windows"
-$OpamBinUrlBase = "https://github.com/ocaml/opam/releases/download/"
-$OpamBin = "opam-${Tag}-${Arch}-${OS}.exe"
-$OpamBinUrl = "${OpamBinUrlBase}${Tag}/${OpamBin}"
-
 Function CheckSHA512 {
   param (
-    [string]$OpamBinLoc
+    [string]$OpamBinTmpLoc,
+    [string]$OpamBinName
   )
 
-  $Hash = (CertUtil -hashfile "$OpamBinLoc" SHA512)[1]
-  $HashTarget = BinSHA512 -OpamBin "$OpamBin"
+  $Hash = (CertUtil -hashfile "$OpamBinTmpLoc" SHA512)[1]
+  $HashTarget = BinSHA512 -OpamBinName "$OpamBinName"
 
   if ("$Hash" -ne "$HashTarget") {
     throw "Checksum does not match"
@@ -48,29 +42,38 @@ Function CheckSHA512 {
 
 Function DownloadAndCheck {
   param (
-    [string]$OpamBinLoc
+    [string]$OpamBinUrl,
+    [string]$OpamBinTmpLoc,
+    [string]$OpamBinName
   )
 
-  Start-BitsTransfer -Source "$OpamBinUrl" -Destination "$OpamBinLoc"
-  CheckSHA512 -OpamBinLoc "$OpamBinLoc"
+  Start-BitsTransfer -Source "$OpamBinUrl" -Destination "$OpamBinTmpLoc"
+  CheckSHA512 -OpamBinTmpLoc "$OpamBinTmpLoc" -OpamBinName "$OpamBinName"
 }
 
 if ($Dev) {
   $Version = $DevVersion
 }
 
-$OpamBinLoc = "$Env:TMP\$OpamBin"
-DownloadAndCheck -OpamBinLoc "$OpamBinLoc"
+$Tag = $Version -creplace "~", "-"
+$Arch = GetArch
+$OS = "windows"
+$OpamBinUrlBase = "https://github.com/ocaml/opam/releases/download/"
+$OpamBinName = "opam-${Tag}-${Arch}-${OS}.exe"
+$OpamBinUrl = "${OpamBinUrlBase}${Tag}/${OpamBinName}"
+
+$OpamBinTmpLoc = "$Env:TMP\$OpamBinName"
+DownloadAndCheck -OpamBinUrl "$OpamBinUrl" -OpamBinTmpLoc "$OpamBinTmpLoc" -OpamBinName "$OpamBinName"
 
 $OpamBinDir = Read-Host "## Where should it be installed? [$DefaultBinDir]"
 if ($OpamBinDir -eq "") {
   $OpamBinDir = $DefaultBinDir
 }
-# TODO: check if OpamBinDir or OpamBinLoc contains single quotes
+# TODO: check if OpamBinDir or OpamBinTmpLoc contains single quotes
 
 if ( -not (Test-Path -Path "$OpamBinDir" -PathType Container) ) {
   Start-Process -FilePath powershell -Verb RunAs -ArgumentList ('-Command', "New-Item '$OpamBinDir' -Type Directory -Force")
 }
 
-Start-Process -FilePath powershell -Verb RunAs -ArgumentList ('-Command', "Move-Item -Path '$OpamBinLoc' -Destination '$OpamBinDir\opam.exe'")
+Start-Process -FilePath powershell -Verb RunAs -ArgumentList ('-Command', "Move-Item -Path '$OpamBinTmpLoc' -Destination '$OpamBinDir\opam.exe'")
 # TODO: modify Path
