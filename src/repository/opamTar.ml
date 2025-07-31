25ac4b61441c821863d4fd8cfcbb6accdd0d961a
Kate <kit-ty-kate@exn.st>
2025-07-31 19:52:31 +0100

    Add OpamTar

---
+++ src/repository/opamTar.ml
@@ -0,0 +1,50 @@
+(**************************************************************************)
+(*                                                                        *)
+(*    Copyright 2025 Kate Deplaix                                         *)
+(*                                                                        *)
+(*  All rights reserved. This file is distributed under the terms of the  *)
+(*  GNU Lesser General Public License version 2.1, with the special       *)
+(*  exception on linking described in the file LICENSE.                   *)
+(*                                                                        *)
+(**************************************************************************)
+
+open Tar.Syntax
+
+let rec safe_read fd buf off len =
+  try Unix.read fd buf off len
+  with Unix.Unix_error (Unix.EINTR, _, _) -> safe_read fd buf off len
+
+let rec run : type a. Unix.file_descr -> (a, _, _) Tar.t -> a = fun fd -> function
+  | Tar.Read len ->
+      let b = Bytes.create len in
+      let read = safe_read fd b 0 len in
+      if read = 0 then
+        failwith "unexpected end of file"
+      else if len = (read : int) then
+        Bytes.unsafe_to_string b
+      else
+        Bytes.sub_string b 0 read
+  | Tar.Really_read len ->
+      let rec loop fd buf offset len =
+        if offset < (len : int) then
+          let n = safe_read fd buf offset (len - offset) in
+          if n = 0 then
+            failwith "unexpected end of file"
+          else
+            loop fd buf (offset + n) len
+      in
+      let buf = Bytes.create len in
+      loop fd buf 0 len;
+      Bytes.unsafe_to_string buf
+  | Tar.Return (Ok x) -> x
+  | Tar.Return (Error _) -> failwith "something's gone wrong"
+  | Tar.High _ | Tar.Write _ | Tar.Seek _ -> assert false
+  | Tar.Bind (x, f) -> run fd (f (run fd x))
+
+let fold f acc fd =
+  let go ?global:_ hdr acc =
+    let* content = Tar.really_read (Int64.to_int hdr.Tar.Header.file_size) in
+    let acc = f acc hdr content in
+    Tar.return (Ok acc)
+  in
+  run fd (Tar_gz.in_gzipped (Tar.fold go acc))
