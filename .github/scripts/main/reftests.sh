3663f5de36eb980a0e5a10f7c1ccbacdbad40262
Kate <kit-ty-kate@exn.st>
2026-04-23 10:51:26 +0100

    fixuplast

--- .github/scripts/main/reftests.sh
+++ .github/scripts/main/reftests.sh
@@ -23,7 +23,7 @@ export PATH="$PREFIX/bin:$PATH"
 git clone https://codeberg.org/kit-ty-kate/micro_httpd
 pushd micro_httpd
 dune build -p micro_httpd
-dune install -p micro_httpd --destdir "$PREFIX"
+dune install -p micro_httpd --prefix "$PREFIX"
 popd
 rm -rf micro_httpd
 
